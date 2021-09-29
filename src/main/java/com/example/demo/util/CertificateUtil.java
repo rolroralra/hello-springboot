9a20d8468b946ec7fd0fb6bbc2c68465cd5d00c2
rolroralra <rolroralra@gmail.com>
2021-09-30 03:06:44 +0900

    refactor: apply "dockerfile-maven-plugin" and replacement "sun.misc.BASE64Encoder"

--- src/main/java/com/example/demo/util/CertificateUtil.java
+++ src/main/java/com/example/demo/util/CertificateUtil.java
@@ -2,8 +2,6 @@ package com.example.demo.util;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import sun.misc.BASE64Encoder;
-import sun.security.provider.X509Factory;
 
 import javax.net.ssl.*;
 import java.io.File;
@@ -12,56 +10,49 @@ import java.io.InputStream;
 import java.security.KeyStore;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.Base64;
 
 @Slf4j
 @Component("CertificateUtil")
 public class CertificateUtil {
+    private static final String BEGIN_CERT = "-----BEGIN CERTIFICATE-----";
+    private static final String END_CERT = "-----END CERTIFICATE-----";
     private static final char[] HEXDIGITS = "0123456789abcdef".toCharArray();
     private final char FILE_SEPERATOR_CHAR = File.separatorChar;
     private final char[] PASSPHRASE = "changeit".toCharArray();
 
     public String getCertificate(String host, int port) throws Exception {
-        try {
-            File dir = new File(System.getProperty("java.home") + FILE_SEPERATOR_CHAR + "lib" + FILE_SEPERATOR_CHAR + "security");
-            File file = new File(dir, "cacerts");
-            InputStream in = new FileInputStream(file);
-            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            keyStore.load(in, PASSPHRASE);
-            in.close();
 
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-            trustManagerFactory.init(keyStore);
+        File dir = new File(System.getProperty("java.home") + FILE_SEPERATOR_CHAR + "lib" + FILE_SEPERATOR_CHAR + "security");
+        File file = new File(dir, "cacerts");
+        InputStream in = new FileInputStream(file);
+        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        keyStore.load(in, PASSPHRASE);
+        in.close();
 
-            X509TrustManager defaultTrustManager = (X509TrustManager) trustManagerFactory.getTrustManagers()[0];
-            SavingTrustManager trustManager = new SavingTrustManager(defaultTrustManager);
-            sslContext.init(null, new TrustManager[]{trustManager}, null);
-            SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init(keyStore);
 
-            SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket(host, port);
-            sslSocket.setSoTimeout(10000);
-            sslSocket.startHandshake();
-            sslSocket.close();
+        X509TrustManager defaultTrustManager = (X509TrustManager) trustManagerFactory.getTrustManagers()[0];
+        SavingTrustManager trustManager = new SavingTrustManager(defaultTrustManager);
+        sslContext.init(null, new TrustManager[]{trustManager}, null);
+        SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
 
-            X509Certificate[] chain = trustManager.chain;
-            if (chain == null) {
-                log.error("Could not obtain server certificate chain");
-                throw new Exception("Could not obtain server certificate chain");
-            }
+        SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket(host, port);
+        sslSocket.setSoTimeout(10000);
+        sslSocket.startHandshake();
+        sslSocket.close();
 
-            X509Certificate certificate = chain[0];
-
-            BASE64Encoder encoder = new BASE64Encoder();
-
-            return X509Factory.BEGIN_CERT +
-                    "\n" +
-                    encoder.encode(certificate.getEncoded()) +
-                    X509Factory.END_CERT +
-                    "\n";
-
-        } catch (Exception e) {
-            throw e;
+        X509Certificate[] chain = trustManager.chain;
+        if (chain == null) {
+            log.error("Could not obtain server certificate chain");
+            throw new Exception("Could not obtain server certificate chain");
         }
+
+        X509Certificate certificate = chain[0];
+
+        return String.format("%s\n%s\n%s\n", BEGIN_CERT, Base64.getMimeEncoder().encodeToString(certificate.getEncoded()), END_CERT);
     }
 
     public static String byteArrayToHexString(byte[] bytes) {
@@ -88,7 +79,7 @@ public class CertificateUtil {
             //throw new UnsupportedOperationException();
         }
 
-        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        public void checkClientTrusted(X509Certificate[] chain, String authType) {
             throw new UnsupportedOperationException();
         }
 
